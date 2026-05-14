427b8abb67a98e4850e0f3e43679b3e8164015a2
Passion <passionlin@zuyou.com.tw>
2026-05-14 18:23:39 +0800

    feat: PWA manifest and service worker

---
+++ icons/README.txt
@@ -0,0 +1,3 @@
+Replace icon-192.png and icon-512.png with real PNG icons.
+You can generate them at https://favicon.io/favicon-generator/
+Use a book emoji or your preferred icon.
---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'learning-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/app.css',
+  './js/app.js',
+  './js/db.js',
+  './js/utils.js',
+  './js/router.js',
+  './js/views/dashboard.js',
+  './js/views/form.js',
+  './js/views/library.js',
+  './js/views/detail.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
