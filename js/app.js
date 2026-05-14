67776ae1dc287c37a957c58dd9ee1125ae8732e8
Passion <passionlin@zuyou.com.tw>
2026-05-14 18:00:07 +0800

    feat: project scaffold - index.html, app.css, app.js

---
+++ js/app.js
@@ -0,0 +1,24 @@
+import { initDB } from './db.js';
+import { initRouter } from './router.js';
+import { renderDashboard } from './views/dashboard.js';
+import { renderLibrary } from './views/library.js';
+import { renderForm } from './views/form.js';
+import { renderDetail } from './views/detail.js';
+
+async function main() {
+  await initDB();
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch(() => {});
+  }
+
+  initRouter({
+    dashboard: () => renderDashboard(),
+    library:   () => renderLibrary(),
+    add:       () => renderForm(),
+    edit:      (params) => renderForm(params.id),
+    detail:    (params) => renderDetail(params.id),
+  });
+}
+
+main();
